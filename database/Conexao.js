7890d9613332a69b0278808194ddcc92c69e90c4
gabrieleugenio1 <101233631+gabrieleugenio1@users.noreply.github.com>
2023-08-20 18:43:06 -0300

    Atualização das dependências

--- database/Conexao.js
+++ database/Conexao.js
@@ -1,8 +1,10 @@
 const Sequelize = require("sequelize");
 
-const Conexao = new Sequelize({
-  dialect: "sqlite", 
-  storage: "./database/eservices.sqlite"
+const Conexao = new Sequelize(process.env.DATABASE, process.env.DB_NOME, process.env.DB_SENHA,{
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  dialect: process.env.DB_DIALECT,
+  timezone: process.env.DB_TIMEZONE,
 });
 
 module.exports = {Conexao, Sequelize};
--- routes/index.js
+++ routes/index.js
@@ -8,5 +8,5 @@ module.exports = app =>{
         principal,
         geral,
         autenticacoes
-        )
-}
+    );
+};
